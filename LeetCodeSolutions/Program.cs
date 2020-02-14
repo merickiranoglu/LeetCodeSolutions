36872cd3102ced1e079732ba1417302a23340ade
Meriç Kıranoğlu <mrckiranoglu@hotmail.com>
2020-02-14 13:34:22 +0300

    Add project files.

---
+++ LeetCodeSolutions/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    class Program
+    {
+        enum eSolutions
+        {
+            TwoSum = 0
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, This environment is created to check LeetCode solutions.");
+
+            Console.WriteLine("Select Solution to Test:");
+
+            for (int i = 0; i < Enum.GetNames(typeof(eSolutions)).Length; i++)
+            {
+                Console.WriteLine(i.ToString() + " : " + (eSolutions)i);
+            }
+
+            int selection = Convert.ToInt32(Console.ReadLine());
+            var solution = (eSolutions) selection;
+
+
+            switch (solution)
+            {
+                case eSolutions.TwoSum:
+                    TestTwoSum();
+                    break;
+                default:
+                    break;
+            }
+
+
+        }
+
+        private static void TestTwoSum()
+        {
+            Console.WriteLine("Test of Two Sum Solution is started.");
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+
+            string testArray = "Test Array: {";
+            for (int i = 0; i < nums.Length-1 ; i++)
+            {
+                testArray += nums[i] + ",";
+            }
+            testArray += nums[nums.Length - 1] + "}";
+
+            Console.WriteLine(testArray);
+            Console.WriteLine("Target value: " + target.ToString());
+
+            var result = TwoSumSolution.TwoSum(nums, target);
+            Console.WriteLine("Result: Indices of the values are; {" + result[0].ToString() + "," + result[1].ToString() + "}");
+            Console.ReadLine();
+        }
+    }
+}
+
+
---
+++ LeetCodeSolutions/TwoSumSolution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions
+{
+    public class TwoSumSolution
+    {
+        
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int[] solution = { 0, 0 };
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                solution[0] = nums[i];
+
+                if (solution[0] > 9) { continue; }
+
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    solution[1] = nums[j];
+
+                    if (solution[0] == solution[1]) { continue; }
+
+                    if (solution[0] + solution[1] == 9)
+                    {
+                        int[] indices = { i, j };
+                        return indices;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+            return solution;
+        }
+    }
+
+}
